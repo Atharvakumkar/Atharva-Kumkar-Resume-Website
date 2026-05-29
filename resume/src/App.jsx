de235c377cb8cd00b929024a38f9d744987c6980
Atharvakumkar <kumkaratharva8@gmail.com>
2026-05-29 12:32:48 +0530

    Main App Routing

--- resume/src/App.jsx
+++ resume/src/App.jsx
@@ -1,12 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Header from "./components/header";
+import HomeHeader from "./components/homeHeader";
+
+import HomePage from "./pages/homepage";
+import AboutPage from "./pages/aboutPage";
+import CertificatesPage from "./pages/certificatesPage";
+import ContactPage from "./pages/contactPage";
+import ProjectsPage from "./pages/projectsPage";
 
 function App() {
     return(
-        <>
-        <div className="bg-black min-h-screen">
-        <Header/>
-        </div>
-        </>
+        <BrowserRouter>
+            <div className="min-h screen bg-black text-white">
+                <Routes>
+                    <Route path ='/' element={<HomePage />} />
+                    <Route path ='/about' element={<AboutPage />} />
+                    <Route path ='/projects' element={<ProjectsPage />} />
+                    <Route path ='/certificates' element={<CertificatesPage />} />
+                    <Route path ='/contact' element={<ContactPage />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
